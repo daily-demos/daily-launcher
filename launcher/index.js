aa717642c890e9e625530460d5128933721a355f
Liza <65890040+lazeratops@users.noreply.github.com>
2023-12-12 10:26:50 +0100

    Launcher improvements (#1)
    * Launcher improvments
    
    * Commit to test branch deploy
    
    * remove dist
    
    * Add mac configuration
    
    * Settings to ask permission on mac
    
    * Notarizing the app
    
    * Fixing permissions after notarize the app.
    
    * Making explicitly that the notarize docs is only for Apple
    
    ---------
    
    Co-authored-by: Filipi Fuchter <filipi87@gmail.com>

--- launcher/index.js
+++ launcher/index.js
@@ -1,16 +1,68 @@
-const defaultRoomURL = "https://lizashul.daily.co/hacktheplanet"
+const defaultRoomURL = "https://YOUR-DOMAIN.daily.co/YOUR-ROOM-NAME"
+const hiddenClassName = "hidden";
 
 window.addEventListener("DOMContentLoaded", () => {
 
-    // Get query parameters
+    const roomURL = getRoomURL();
+    console.log("roomurl:", roomURL)
+    configureBrowserLaunch(roomURL);
+    configureDesktopLaunch(roomURL);
+
+    configureDownloadButtons();
+
+    // Set up download buttons
+    const parser = new UAParser(); // you need to pass the user-agent for nodejs
+    const parserResults = parser.getResult();
+
+    osName = parserResults.os.name;
+    // Make all downloads visible by default, but if we detect an OS in the
+    // user agent, hide the other download button.
+    if (osName === "Mac OS") {
+        const win = document.getElementById("win")
+        win.classList.add(hiddenClassName)
+    } else if (osName === "Windows") {
+        const mac = document.getElementById("mac")
+        mac.classList.add(hiddenClassName)
+    }
+
+});
+
+function configureDownloadButtons() {
+    // Mac
+    const macArm = document.getElementById("macARMDownload")
+    macArm.href = "https://daily-video-launcher.s3.us-west-2.amazonaws.com/daily-video-1.0.0-mac-arm64.dmg"
+
+    const macIntel = document.getElementById("macIntelDownload")
+    macIntel.href = "https://daily-video-launcher.s3.us-west-2.amazonaws.com/daily-video-1.0.0-mac-intel.dmg"
+
+    const win = document.getElementById("windowsDownload")
+    win.href = "https://daily-video-launcher.s3.us-west-2.amazonaws.com/daily-video-1.0.0-windows.exe"
+
+    const dl = document.getElementById("download")
+    dl.classList.remove(hiddenClassName)
+
+}
+
+function getRoomURL() {
     const urlParams = new URLSearchParams(window.location.search);
     let roomURL = urlParams.get("roomURL");
     if (!roomURL) {
         roomURL = defaultRoomURL
     }
+    return roomURL
+}
 
-    const p = document.getElementById("msg")
+function configureDesktopLaunch(roomURL) {
     const url = `daily-vid://?roomURL=${roomURL}`
-    p.innerHTML = `Daily Video should open automatically. If it doesn't, <a href="${url}">click here</a> to launch.`
-    window.location.href= url
-});
+    const btn = document.getElementById("launchDesktop")
+    btn.onclick = () => {
+        window.location.href = url;
+    }
+    window.location.href = url;
+}
+
+function configureBrowserLaunch(roomURL) {
+    const a = document.getElementById("launchBrowser")
+    a.href = roomURL;
+}
+
